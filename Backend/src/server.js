9b52fe943bea8be9dc3fb5f64d7927255306130f
ayushsingh-0710 <ayushsingh51833@gmail.com>
2026-04-20 14:05:30 +0530

    fix server path and cleanup

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
+const connectDB = require('./config/db'); 
 const authRouter = require('./routers/auth.router');
 const adminRouter = require('./routers/admin.router');
 const chatbotRouter = require('./routers/chatbot.router');
